abc9b0e8646286211e21127859ab4facb3fdf1f8
megan <msmeganmcmanus@gmail.com>
2021-02-27 10:40:47 -0800

    complete colorfactory app

---
+++ 04_patternex/colorfactory/src/ColorForm.js
@@ -0,0 +1,62 @@
+
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+
+function ColorForm({ addColor }) {
+	const INITIAL_COLOR = {
+		name: '',
+		color: ''
+	};
+
+	const [formData, setFormData] = useState(INITIAL_COLOR);
+	const history = useHistory();
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormData((formData) => ({
+			...formData,
+			[name]: value
+		}));
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		addColor({ [formData.name]: formData.color });
+		setFormData(INITIAL_COLOR);
+		history.push('/colors');
+	};
+	return (
+		<>
+			<h1>ADD A COLOR</h1>
+			<div className='ColorForm'>
+				<form onSubmit={handleSubmit}>
+                    <p>Please enter a Name for your color and then pick the corresponding shade using the RGP color picker.  Both MUST be filled out.</p>
+					<label htmlFor='name'>Color Name</label>
+					<input
+						id='name'
+						type='text'
+						name='name'
+						placeholder='Enter Name...'
+						value={formData.name}
+						onChange={handleChange}
+					/>
+
+					<label htmlFor='color'>Color Value</label>
+					<input
+						id='color'
+						type='color'
+						name='color'
+						placeholder='Enter Color...'
+						onChange={handleChange}
+						value={formData.color}
+					/>
+
+					<button>Add Color</button>
+				</form>
+			</div>
+		</>
+	);
+}
+
+export default ColorForm;

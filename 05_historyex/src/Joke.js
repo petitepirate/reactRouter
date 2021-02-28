8b5950ef125f6abe208abc5dbfbbc00ddc0980ca
megan <msmeganmcmanus@gmail.com>
2021-02-28 11:40:05 -0800

    complete joke app

--- 05_historyex/src/Joke.js
+++ 05_historyex/src/Joke.js
@@ -1,27 +1,68 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Joke.css";
 
-function Joke({ vote, votes, text, id }) {
-  const upVote = () => vote(id, +1);
-  const downVote = () => vote(id, -1);
+class Joke extends Component {
+  constructor(props) {
+    super(props);
+    this.upVote = this.upVote.bind(this);
+    this.downVote = this.downVote.bind(this);
 
-  return (
-    <div className="Joke">
-      <div className="Joke-votearea">
-        <button onClick={upVote}>
-          <i className="fas fa-thumbs-up" />
-        </button>
+  }
 
-        <button onClick={downVote}>
-          <i className="fas fa-thumbs-down" />
-        </button>
+  upVote() {
+    this.props.vote(this.props.id, +1);
+  }
 
-        {votes}
+  downVote() {
+    this.props.vote(this.props.id, -1);
+  }
+
+
+  render() {
+    return (
+      <div className={`Joke`}>
+        <div className="Joke-votearea">
+          <button onClick={this.upVote}>
+            <i className="fas fa-thumbs-up" />
+          </button>
+
+          <button onClick={this.downVote}>
+            <i className="fas fa-thumbs-down" />
+          </button>
+          {this.props.votes}
+        </div>
+
+        <div className="Joke-text">{this.props.text}</div>
       </div>
-
-      <div className="Joke-text">{text}</div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Joke;
+
+
+// ************** STARTER CODE - using Hooks *****************************
+// function Joke({ vote, votes, text, id }) {
+//   const upVote = () => vote(id, +1);
+//   const downVote = () => vote(id, -1);
+
+//   return (
+//     <div className="Joke">
+//       <div className="Joke-votearea">
+//         <button onClick={upVote}>
+//           <i className="fas fa-thumbs-up" />
+//         </button>
+
+//         <button onClick={downVote}>
+//           <i className="fas fa-thumbs-down" />
+//         </button>
+
+//         {votes}
+//       </div>
+
+//       <div className="Joke-text">{text}</div>
+//     </div>
+//   );
+// }
+
+// export default Joke;
